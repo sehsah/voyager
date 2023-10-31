7b15583d0ddc02f48a2f38818d16645c95a9759e
Mahmoud sehsah <mahmoudsehsah36@gmail.com>
2023-10-31 11:12:14 +0200

    add ar files

--- resources/views/menus/browse.blade.php
+++ resources/views/menus/browse.blade.php
@@ -1,10 +1,10 @@
 @extends('voyager::master')
 
-@section('page_title', __('voyager::generic.viewing').' '.$dataType->getTranslatedAttribute('display_name_plural'))
+@section('page_title', __('voyager::generic.viewing').' '.$dataType->title)
 
 @section('page_header')
     <h1 class="page-title">
-        <i class="voyager-list-add"></i> {{ $dataType->getTranslatedAttribute('display_name_plural') }}
+        <i class="voyager-list-add"></i> {{ $dataType->title }}
         @can('add',app($dataType->model_name))
             <a href="{{ route('voyager.'.$dataType->slug.'.create') }}" class="btn btn-success">
                 <i class="voyager-plus"></i> {{ __('voyager::generic.add_new') }}
